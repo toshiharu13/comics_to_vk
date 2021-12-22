48cc90994ee232cfc3713339fb9226d5f9bd8e8e
Boyko Alexey <elorian@yandex.ru>
2021-12-22 17:49:56 +0300

    add get vk groups func

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ import os
 from environs import Env
 
 
+def get_vk_request(url, params, method):
+    full_uri = f'{url}/{method}'
+    response = requests.get(full_uri, params=params)
+    response.raise_for_status()
+    logging.info(response.text)
+    return response.json()
+
+
 def get_comics(url):
     response = requests.get(url)
     response.raise_for_status()
@@ -46,6 +54,13 @@ if __name__ == '__main__':
     comics_folder = Path.cwd()/'comics'
     Path(comics_folder).mkdir(parents=True, exist_ok=True)
     vk_app_id = env.str('VK_APP_ID')
-
+    vk_api_uri = 'https://api.vk.com/method'
+    vk_url_params = {
+        'access_token': env.str('VK_TOKEN'),
+        'user_id': 562778,
+        'v': '5.131'
+    }
+    method = 'groups.get'
+    print(get_vk_request(vk_api_uri, vk_url_params, method))
 
 
