8e93b70bac18ba3baf2f46ad545fb5660650d997
Boyko Alexey <elorian@yandex.ru>
2021-12-22 15:19:51 +0300

    add comment to terminal

--- main.py
+++ main.py
@@ -10,10 +10,12 @@ def get_comics(url):
     response.raise_for_status()
     logging.info(response.json())
     comics_url = response.json()['img']
+    commics_comment = response.json()['alt']
     file_name = get_filename_from_url(comics_url)
     copy_destination = Path.cwd()/'comics'/file_name
     download_comics(copy_destination, comics_url)
-    logging.info(comics_url, file_name)
+    logging.info(f'download {comics_url}')
+    return commics_comment
 
 
 def download_comics(destination, url):
@@ -39,5 +41,5 @@ if __name__ == '__main__':
     current_comics_json_url = 'https://xkcd.com/info.0.json'
     comics_folder = Path.cwd()/'comics'
     Path(comics_folder).mkdir(parents=True, exist_ok=True)
-    get_comics(current_comics_json_url)
+    print(get_comics(current_comics_json_url))
 
